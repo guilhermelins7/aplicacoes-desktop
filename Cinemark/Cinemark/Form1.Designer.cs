3f45095779afde8d14c2c123ed2500bdbfcca674
unisanta <unisanta@santacecilia.br>
2025-04-25 21:10:41 -0300

    feat: Adicionar atividade Cinemark

---
+++ Cinemark/Cinemark/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Cinemark
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Faturamento = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Faturamento
+            // 
+            this.Faturamento.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Faturamento.Location = new System.Drawing.Point(366, 846);
+            this.Faturamento.Name = "Faturamento";
+            this.Faturamento.Size = new System.Drawing.Size(215, 46);
+            this.Faturamento.TabIndex = 0;
+            this.Faturamento.Text = "Faturamento";
+            this.Faturamento.UseVisualStyleBackColor = true;
+            this.Faturamento.Click += new System.EventHandler(this.Faturamento_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 904);
+            this.Controls.Add(this.Faturamento);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Faturamento;
+    }
+}
+
---
+++ Cinemark/Cinemark/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cinemark
+{
+    public partial class Form1 : Form
+    {
+        Button[,] lugares = new Button[10, 20];
+        char letra = 'A'; // 0100 0001 em decimal 65 e em hexa 41
+        float faturamento = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for(int coluna = 0; coluna < 10; coluna++)
+            {
+                for (int linha = 0; linha < 20; linha++)
+                {
+                    lugares[coluna, linha] = new Button();
+                    lugares[coluna, linha].Size = new Size(90, 40);
+                    lugares[coluna, linha].Location = new Point(100 * coluna + 25, 40 * linha + 25);
+                    int cadeiraNumero = linha + 1;
+                    lugares[coluna, linha].Text = letra.ToString() + cadeiraNumero.ToString();
+                    lugares[coluna, linha].BackColor = Color.Green;
+                    Controls.Add(lugares[coluna, linha]);
+
+                    lugares[coluna, linha].Click += Botao_Clickado;
+                }
+
+                ++letra;
+            }
+        }
+        private void Botao_Clickado(object sender, EventArgs e)
+        {
+            Button botaoClicado = sender as Button;
+            if (botaoClicado.BackColor == Color.Green)
+            {
+                DialogResult resultado = MessageBox.Show(
+                    "Deseja reservar este lugar?",
+                    "Confirmação de Reserva",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question
+                );
+
+                if (resultado == DialogResult.Yes)
+                {
+                    botaoClicado.BackColor = Color.Red;
+                    faturamento += 20;
+                }
+            }
+            else
+            {
+                DialogResult resultado = MessageBox.Show(
+                    "Deseja cancelar a reserva deste lugar?",
+                    "Cancelar Reserva",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Warning
+                );
+
+                if (resultado == DialogResult.Yes)
+                {
+                    botaoClicado.BackColor = Color.Green;
+                    faturamento -= 20;
+                }
+            }
+
+        }
+
+        private void Faturamento_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("O faturamento foi de R$" + faturamento.ToString());
+        }
+    }
+}
